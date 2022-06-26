9b80446886f35ac79e0bcbcd2cdb586a7f28dcb9
pouriapourrashidi <p.pourrashidy@gmail.com>
2022-06-26 23:43:52 +0430

    adding the first addition of the files

---
+++ script.js
@@ -0,0 +1,13 @@
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var css=document.querySelector("h3");
+var body=document.getElementById("bodyhere");
+
+function setbackground(){
+	body.style.background="linear-gradient(to right,"+color1.value
+	+", " + color2.value + ")";
+	css.textContent = body.style.background+";";
+}
+
+color1.addEventListener("input",setbackground);
+color2.addEventListener("input",setbackground);
